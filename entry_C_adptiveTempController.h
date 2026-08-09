2450ab6f23fa43dc076fa64d6464ab03efa80a06
ch7lin-cyber <ch7.lin@gmail.com>
2026-08-09 16:36:07 +0800

    Path Fix-2-B: add application wrapper header

---
+++ entry_C_adptiveTempController.h
@@ -0,0 +1,33 @@
+/***************************************************************
+Description :
+    Application wrapper API for adaptive temperature controller.
+
+Purpose:
+    Keep platform/application entry points separate from the reusable
+    PID_ControllerSrc library interface.
+***************************************************************/
+#ifndef ENTRY_C_ADPTIVE_TEMP_CONTROLLER_H_
+#define ENTRY_C_ADPTIVE_TEMP_CONTROLLER_H_
+
+#include <stdint.h>
+#include "ssm_std_define.h"
+#include "PID_ControllerSrc/FB_C_adaptive_temperature_controller.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+MY_API void Heater_Control_Init(void);
+
+MY_API void Heater_myAdptiveControl(
+    int16_t input_pv,
+    int16_t input_sv,
+    int32_t *output_pid_out,
+    int32_t *output_ff_pwm,
+    int32_t *output_ff_offset);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
