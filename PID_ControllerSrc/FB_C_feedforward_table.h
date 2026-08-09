c00148df452d9faa908bba3d768438ebb14832ad
ch7lin-cyber <ch7.lin@gmail.com>
2026-08-09 16:29:17 +0800

    Path Fix-2-A: make feedforward table header self-contained

--- PID_ControllerSrc/FB_C_feedforward_table.h
+++ PID_ControllerSrc/FB_C_feedforward_table.h
@@ -1,6 +1,7 @@
 #ifndef SSM_STD_FB_APP_USER_FEEDFORWARD_TABLE_CODE_H_
 #define SSM_STD_FB_APP_USER_FEEDFORWARD_TABLE_CODE_H_
 
+#include <stdint.h>
 #include "FB_C_control_type.h"
 #include "FB_C_parameter.h"
 
