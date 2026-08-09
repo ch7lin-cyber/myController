4576d67853d16660f291f96b44e717eb14cdc48e
ch7lin-cyber <ch7.lin@gmail.com>
2026-08-09 16:26:27 +0800

    Path Fix: make MY_API cross-platform for DLL, shared and static builds

--- ssm_std_define.h
+++ ssm_std_define.h
@@ -1,8 +1,6 @@
-
 /**
  * @file ssm_std_define.h
- * @brief Main include file for the SSM FB dein.
- *
+ * @brief Common platform definitions for the SSM FB library.
  */
 
 #ifndef SSM_STD_FB_DEFINE_H_
@@ -11,32 +9,46 @@
 #include <stdbool.h>  /* NOLINT */
 #include <stdint.h>   /* NOLINT */
 
-
-
 #define SUCCESS 0x00000000
 #define FAIL    0x00000001
 
+/*
+====================================================
+ Cross-platform public API visibility
+====================================================
 
+Windows DLL build:
+    define SSM_FB_BUILD_DLL
 
-// 在 PC 編譯時，確保函式可以被外部呼叫
-#if defined(_WIN32) || defined(_WIN32_)
-    #define MY_API __declspec(dllexport)
+Windows DLL consumer:
+    define SSM_FB_USE_DLL
+
+Linux / GCC shared-library build:
+    define SSM_FB_BUILD_SHARED
+
+Static library, executable, MCU and other platforms:
+    no additional define is required.
+====================================================
+*/
+
+#if defined(_WIN32) || defined(__CYGWIN__)
+
+    #if defined(SSM_FB_BUILD_DLL)
+        #define MY_API __declspec(dllexport)
+    #elif defined(SSM_FB_USE_DLL)
+        #define MY_API __declspec(dllimport)
+    #else
+        #define MY_API
+    #endif
+
+#elif defined(__GNUC__) && defined(SSM_FB_BUILD_SHARED)
+
+    #define MY_API __attribute__((visibility("default")))
+
 #else
+
     #define MY_API
+
 #endif
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #endif  // SSM_STD_FB_DEFINE_H_
