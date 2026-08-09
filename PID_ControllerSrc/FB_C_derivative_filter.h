38bd7f315b166a3c76fb2bf6a5ef14fb2d8933f4
ch7lin-cyber <ch7.lin@gmail.com>
2026-08-09 16:53:20 +0800

    Standardize C++ compatibility block in derivative filter header

--- PID_ControllerSrc/FB_C_derivative_filter.h
+++ PID_ControllerSrc/FB_C_derivative_filter.h
@@ -5,9 +5,12 @@
 #include "FB_C_control_type.h"
 #include "FB_C_parameter.h"
 
+//------------------------------------------------------------------------------------//
+// C++ compatibility - DO NOT DELETE
 #ifdef __cplusplus
 extern "C" {
 #endif
+//------------------------------------------------------------------------------------//
 
 typedef struct
 {
@@ -21,7 +24,11 @@ void app_fb_d_filter_init(APP_FB_D_FILTER_T *fb, int32_t alpha);
 void app_fb_d_filter_reset(APP_FB_D_FILTER_T *fb);
 int32_t app_fb_d_filter_run(APP_FB_D_FILTER_T *fb, int32_t pv);
 
+//------------------------------------------------------------------------------------//
+// C++ compatibility - DO NOT DELETE
 #ifdef __cplusplus
 }
 #endif
+//------------------------------------------------------------------------------------//
+
 #endif
