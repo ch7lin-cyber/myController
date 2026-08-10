18e794c8379d5e5cc9f3c356be4d585e9f18c9d9
ch7lin-cyber <ch7.lin@gmail.com>
2026-08-10 13:46:15 +0800

    B4-T2 make application sample time initialization-only

--- entry_C_adptiveTempController.h
+++ entry_C_adptiveTempController.h
@@ -26,15 +26,16 @@ MY_API void Heater_Control_Init(void);
 MY_API void Heater_Control_InitEx(
     const APP_FB_ADAPTIVE_PARAMETER_T *adaptive_parameter);
 
-/* Timing-aware application entry points. sample_time_ms range: 1..6000 ms. */
+/*
+ * Timing-aware initialization. sample_time_ms range: 1..6000 ms.
+ * The value tells the controller its fixed scheduler period and is not changed
+ * while the controller is running.
+ */
 MY_API APP_FB_ERROR Heater_Control_InitTimed(
     uint32_t sample_time_ms);
 
 MY_API APP_FB_ERROR Heater_Control_InitExTimed(
     const APP_FB_ADAPTIVE_PARAMETER_T *adaptive_parameter,
-    uint32_t sample_time_ms);
-
-MY_API APP_FB_ERROR Heater_SetSampleTimeMs(
     uint32_t sample_time_ms);
 
 MY_API uint32_t Heater_GetSampleTimeMs(void);
