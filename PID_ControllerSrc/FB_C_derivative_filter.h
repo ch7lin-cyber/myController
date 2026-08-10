9e7e0959a8eb2792e5f7bae8ec4ce785c3b66ce1
ch7lin-cyber <ch7.lin@gmail.com>
2026-08-10 13:49:39 +0800

    B4-T3 add time-aware derivative filter init

--- PID_ControllerSrc/FB_C_derivative_filter.h
+++ PID_ControllerSrc/FB_C_derivative_filter.h
@@ -15,12 +15,23 @@ extern "C" {
 typedef struct
 {
     int32_t alpha;
+    uint32_t sample_time_ms;
+    uint32_t time_constant_ms;
     int32_t output;
     int32_t pv_previous;
     APP_FB_BOOL initialized;
 } APP_FB_D_FILTER_T;
 
+/* Legacy alpha-based init retained for compatibility. */
 void app_fb_d_filter_init(APP_FB_D_FILTER_T *fb, int32_t alpha);
+
+/* Preferred timing-aware init. Alpha is calculated once during init as:
+ * alpha = tau / (tau + Ts), represented in Q15. */
+APP_FB_ERROR app_fb_d_filter_init_timed(
+    APP_FB_D_FILTER_T *fb,
+    uint32_t sample_time_ms,
+    uint32_t time_constant_ms);
+
 void app_fb_d_filter_reset(APP_FB_D_FILTER_T *fb);
 int32_t app_fb_d_filter_run(APP_FB_D_FILTER_T *fb, int32_t pv);
 
