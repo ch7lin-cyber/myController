8dc758cbc6f92717e3251b0b47facef857d51237
ch7lin-cyber <ch7.lin@gmail.com>
2026-08-10 15:07:26 +0800

    B4-T7 fix derivative filter precision at fast sample times

--- PID_ControllerSrc/FB_C_derivative_filter.h
+++ PID_ControllerSrc/FB_C_derivative_filter.h
@@ -17,6 +17,10 @@ typedef struct
     int32_t alpha;
     uint32_t sample_time_ms;
     uint32_t time_constant_ms;
+
+    /* Filter state is retained in Q15 precision to prevent integer-limit-cycle
+     * residuals when alpha is close to 1 at fast sample times. */
+    int64_t output_q15;
     int32_t output;
     int32_t pv_previous;
     APP_FB_BOOL initialized;
