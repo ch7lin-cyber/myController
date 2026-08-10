b152c4e9238bf8ba6690617797809fd31165ac39
ch7lin-cyber <ch7.lin@gmail.com>
2026-08-10 13:33:43 +0800

    B4 T1 define dynamic sample-time capability

--- PID_ControllerSrc/FB_C_control_type.h
+++ PID_ControllerSrc/FB_C_control_type.h
@@ -26,13 +26,28 @@ typedef enum { APP_FB_STATE_INIT = 0, APP_FB_STATE_IDLE, APP_FB_STATE_RUN, APP_F
 typedef enum { APP_FB_MODE_MANUAL = 0, APP_FB_MODE_AUTO } APP_FB_CONTROL_MODE;
 typedef enum { APP_FB_OK = 0, APP_FB_ERROR_NULL_POINTER, APP_FB_ERROR_SENSOR, APP_FB_ERROR_PARAMETER, APP_FB_ERROR_OUTPUT } APP_FB_ERROR;
 
-#define APP_FB_Q15_ONE              (32768)
-#define APP_FB_CONTROL_PERIOD_MS     (20)
-#define APP_FB_ADAPTIVE_PERIOD       (50)
-#define APP_FB_TEMP_MIN              (0)
-#define APP_FB_TEMP_MAX              (3000)
-#define APP_FB_PWM_MIN               (0)
-#define APP_FB_PWM_MAX               (1000)
+#define APP_FB_Q15_ONE                    (32768)
+
+/*
+ * Controller sample-time capability.
+ *
+ * The application owns the real scheduling period and passes it into the
+ * controller. 20 ms is retained only as the backward-compatible default for
+ * legacy init APIs; it is no longer a fixed controller assumption.
+ */
+#define APP_FB_SAMPLE_TIME_MIN_MS          (1U)
+#define APP_FB_SAMPLE_TIME_MAX_MS          (6000U)
+#define APP_FB_SAMPLE_TIME_DEFAULT_MS      (20U)
+#define APP_FB_SAMPLE_TIME_US_PER_MS       (1000U)
+
+/* Legacy/default alias. New code should use APP_FB_TIMING_PARAMETER_T. */
+#define APP_FB_CONTROL_PERIOD_MS           APP_FB_SAMPLE_TIME_DEFAULT_MS
+
+#define APP_FB_ADAPTIVE_PERIOD             (50)
+#define APP_FB_TEMP_MIN                    (0)
+#define APP_FB_TEMP_MAX                    (3000)
+#define APP_FB_PWM_MIN                     (0)
+#define APP_FB_PWM_MAX                     (1000)
 #define APP_FB_ABS(x) (((x)>=0)?(x):(-(x)))
 #define APP_FB_LIMIT(x,min,max) (((x)<(min))?(min):(((x)>(max))?(max):(x)))
 
